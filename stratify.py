bmr-cymru/stratify

commit aedc4348c2f8d0da129ac550b89b8bb3b53d2739

Require use of a kickstart file for installations

stratify.py
@@ -901,8 +901,12 @@ def main(argv):
 
     _log_info("stratify.py %s - %s" % (_version, _date))
 
-    if args.rescue or args.cleanup:
+    if args.rescue or args.cleanup or args.wipe:
         args.nopartition = True
+    else:
+        if not args.kickstart:
+            _log_error("A kickstart file is required for installation")
+            fail(1)
 
     if args.rescue:
         if args.cleanup:

commit 7cc872471152e47efe2f319a32d9ebb37dfda093

Fix --wipe
Commit aedc434 broke --wipe by force-enabling args.nopartition
(which explicitly conflicts with --wipe): revert that change and
move the wipe action after stopping stratisd.

stratify.py
@@ -901,7 +901,7 @@ def main(argv):
 
     _log_info("stratify.py %s - %s" % (_version, _date))
 
-    if args.rescue or args.cleanup or args.wipe:
+    if args.rescue or args.cleanup:
         args.nopartition = True
     else:
         if not args.kickstart:
@@ -941,13 +941,13 @@ def main(argv):
         _log_error("No target device given!")
         fail(1)
 
-    if args.wipe:
-        # Remove pre-existing stratis pools
-        destroy_pools()
-
     # Stop the Stratis daemon if it is running so that we can wipe any
     # stale data from the target device.
     stop_stratisd()
+
+    if args.wipe:
+        # Remove pre-existing stratis pools
+        destroy_pools()
 
     target = args.target
     pool = args.pool_name