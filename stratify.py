1b8fe0023a762f2636b6c6bf8aa4d54df2473c64
Bryn M. Reeves <bmr@redhat.com>
2025-03-04 19:01:19 +0000

    Remove useless import rename for subprocess.run

--- stratify.py
+++ stratify.py
@@ -3,7 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from subprocess import run as run
+from subprocess import run
 from sys import exit, argv
 from argparse import ArgumentParser
 from os.path import basename, join, exists, isabs
